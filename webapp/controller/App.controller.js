c9b97294b0bca0c93afa3e8f89447d94ac2e878e
Jörn <joern.otte@promos-consult.de>
2019-05-20 16:17:51 +0200

    week 2 unit 2

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"opensap/movies/model/formatter"
-], function(Controller, formatter) {
+	"opensap/movies/model/formatter",
+	"sap/base/Log"
+], function(Controller, formatter, Log) {
 	"use strict";
 
 	return Controller.extend("opensap.movies.controller.App", {
@@ -9,7 +10,27 @@ sap.ui.define([
 		formatter: formatter,
 
 		onInit: function () {
+			Log.info('controller has been initialized');
+		},
 
+		onExit: function () {
+			Log.info('controller will shortly be destroyed');
+		},
+
+		onBeforeRendering: function () {
+			Log.info('The view will shortly be rendered');
+		},
+
+		onAfterRendering: function () {
+			Log.info('controller has been initialized');
+		},
+
+		onPress : function(sValue) {
+			sap.ui.require([
+				'sap/m/MessageToast'
+			], function(MessageToast) {
+				MessageToast.show('searching... ' + sValue);
+			});
 		}
 	});
 });
